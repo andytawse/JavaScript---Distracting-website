78a08a292a5309d660516494e8280e0f04c85223
Andy Tawse <andy.tawse+work@gmail.com>
2025-01-31 15:33:22 +0000

    Randomise movement in advert and restrict to container

--- js/advert.js
+++ js/advert.js
@@ -23,8 +23,6 @@ function animationSetup() {
 
     let game = new Phaser.Game(config);
 
-    console.log('game created');
-
 }
 
 let ad;
@@ -37,18 +35,44 @@ function create() {
         initialize:
 
             function Ad(scene, x, y) {
+                this.scene = scene;
+
+                // Container dimensions
+                this.containerWidth = 300;
+                this.containerHeight = 250;
+
+                // Initial position
                 this.position = new Phaser.Geom.Point(x, y);
-                this.velocity = new Phaser.Geom.Point(50, 50); // Speed in pixels per second
+
+                // Randomize initial velocity (speed and direction)
+                this.velocity = new Phaser.Geom.Point(
+                    Phaser.Math.Between(-150, 150), // Random X velocity between -50 and 50
+                    Phaser.Math.Between(-150, 150)  // Random Y velocity between -50 and 50
+                );
+
                 this.character = scene.add.group().create(x * 48, y * 48, 'character');
                 this.character.setOrigin(0);
+
+                // Adjust character size if needed (to ensure it doesn't go out of bounds due to the image size)
+                this.character.displayWidth = 48; // Adjust to your graphic size
+                this.character.displayHeight = 48; // Adjust to your graphic size
             },
 
         update: function(time, delta) {
-            // Translate position using velocity and time elapsed
-            this.position.x += this.velocity.x * (delta / 1000); // Convert delta ms to seconds
+            // Calculate new position
+            this.position.x += this.velocity.x * (delta / 1000);
             this.position.y += this.velocity.y * (delta / 1000);
 
-            // Update the graphic's position
+            // Check bounds against container width/height
+            if (this.position.x <= 0 || this.position.x + this.character.displayWidth >= this.containerWidth) {
+                this.velocity.x *= -1; // Reverse X direction on collision
+            }
+
+            if (this.position.y <= 0 || this.position.y + this.character.displayHeight >= this.containerHeight) {
+                this.velocity.y *= -1; // Reverse Y direction on collision
+            }
+
+            // Update graphic position
             this.character.setX(this.position.x);
             this.character.setY(this.position.y);
         }
