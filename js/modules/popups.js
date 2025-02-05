8dabbe2fd036b089adfeb72daade9613c644b5d8
Andy Tawse <andy.tawse+work@gmail.com>
2025-02-05 10:58:01 +0000

    Add newsletter popup

--- js/modules/popups.js
+++ js/modules/popups.js
@@ -12,14 +12,33 @@
 const cookieModal = {
     container: document.querySelector('#modal'),
     backdrop: document.querySelector('#modal-backdrop'),
-    close: document.querySelector('#modal-close'),
+    close: document.querySelector('#modal .modal-close'),
+}
+
+const newsletterModal = {
+    container: document.querySelector('#newsletter-modal'),
+    backdrop: document.querySelector('#modal-backdrop'),
+    close: document.querySelector('#newsletter-modal .modal-close'),
 }
 
 /**
  * Start the first modal sequence - the cookie popup.
  */
 function init() {
-    setTimeout(showModal, 500, cookieModal);
+    // Stop any forms in modals from submitting.
+    // Form submission functionality would be dealt with separately.
+    document.querySelector('#modal-backdrop form').addEventListener(
+        'submit',
+        (event) => { return event.preventDefault(); }
+    );
+
+    // Open the first modal.
+    setTimeout(
+        showModal,
+        500,
+        cookieModal,
+        onCookieModalClose // Once the first modal closes, open the second.
+    );
 }
 
 /**
@@ -28,21 +47,47 @@ function init() {
  * @param modalElements
  *   Object containing necessary elements for modal to function e.g.
  *   { backdrop: Element, container: Element, close: Element }
+ *  @param onClose
+ *   Function to call when the modal is closed.
  */
-function showModal(modalElements) {
+function showModal(modalElements, onClose = null) {
     modalElements.backdrop.style.visibility = 'visible';
     modalElements.container.style.visibility = 'visible';
     modalElements.close.addEventListener('click', () => {
-        closeModal(modalElements)
+        closeModal(modalElements, onClose)
     });
 }
 
-function closeModal(modalElements) {
+/**
+ * Complements showModal().
+ *
+ * @param modalElements
+ *   See showModal().
+ * @param onClose
+ *   Function to call when the modal is closed.
+ */
+function closeModal(modalElements, onClose = null) {
     modalElements.backdrop.style.visibility = 'hidden';
     modalElements.container.style.visibility = 'hidden';
     modalElements.close.removeEventListener('click', () => {
         closeModal(modalElements)
     });
+    if (onClose) {
+        onClose();
+    }
+}
+
+/**
+ * Callback for when cookie modal closes.
+ *
+ * Shows the newsletter modal.
+ */
+function onCookieModalClose() {
+    setTimeout(
+        showModal,
+        1000,
+        newsletterModal
+    );
 }
 
 /**
